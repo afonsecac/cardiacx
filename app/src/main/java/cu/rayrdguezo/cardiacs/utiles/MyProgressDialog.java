03fb7acb746357236179e87cdb5dbff31139cbda
Raynel Rodríguez <19raynel94@ecc.cu>
2021-02-21 10:55:16 -0500

    Añadida la interface RecyclerItemClickListener y la clase MyProgressDialog en el paquete utiles

---
+++ app/src/main/java/cu/rayrdguezo/cardiacs/utiles/MyProgressDialog.java
@@ -0,0 +1,32 @@
+package cu.rayrdguezo.cardiacprueba.utiles;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+public class MyProgressDialog {
+
+    ProgressDialog progressDialog;
+
+    public MyProgressDialog(Context context) {
+        this.progressDialog = new ProgressDialog(context);
+    }
+
+    public void mostrarCargando (final String texto){
+        progressDialog.setMessage(texto);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+    }
+
+    public void mostrarCargando (){
+        progressDialog.setMessage("Cargando ...");
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+    }
+
+    public void ocultarCargando (){
+        progressDialog.dismiss();
+    }
+
+}
---
+++ app/src/main/java/cu/rayrdguezo/cardiacs/utiles/RecyclerItemClickListener.java
@@ -0,0 +1,11 @@
+package cu.rayrdguezo.cardiacprueba.utiles;
+
+import android.view.View;
+
+/**
+ * Created by citi on 06/07/2017.
+ */
+
+public interface RecyclerItemClickListener {
+    void onItemClickRecycler(View view, Object obj, int position);
+}
