bab76f76611911808359f8ab6bcb4a6b874a9723
Raynel Rodríguez <19raynel94@gmail.com>
2021-03-24 12:44:55 -0400

    Actualizacion de las efectaciones por renombrar LoginActivity.

--- app/src/main/java/cu/rayrdguezo/cardiacs/FlujoPrincipalFragmentActivity.java
+++ app/src/main/java/cu/rayrdguezo/cardiacs/FlujoPrincipalFragmentActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class FlujoPrincipalFragmentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_flujo_principal_fragment);
     }
 }
